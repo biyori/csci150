d71f889fd3868433485768e65f22f2942dd05692
Kyle A <kyleandrews@mail.fresnostate.edu>
2020-09-22 21:24:37 +0000

    Initial Push

---
+++ Nutrition/Database.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Nutrition
+{
+    class Database
+    {
+        string Server = "tcp:nutrition.database.windows.net,1433";
+        string DB = "Nutrition";
+        string Username = "csci150";
+        string Password = "NutritionPass1";
+        string connectionString = "";
+        SqlConnection mainConnection;
+
+        public Database()
+        {
+            connectionString = @"Server=" + Server + ";Initial Catalog=" + DB + ";Persist Security Info=False;User ID=" + Username + ";Password=" + Password + ";MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            mainConnection = new SqlConnection(connectionString);
+            try
+            {
+                mainConnection.Open();
+                //MessageBox.Show("Connected to database");
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show("Database Error: " + err.Message);
+            }
+        }
+       /* ~Database()
+        {
+            mainConnection.Close();
+        }*/
+
+        public bool checkUserExists(string user)
+        {
+            string sql = "SELECT * from [dbo].[Users] where [username] = @user";
+            using SqlCommand command = new SqlCommand(sql, mainConnection);
+            command.Parameters.AddWithValue("@user", user);
+            var result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                //MessageBox.Show("User already exists try a different name");//Debug
+                result.Close();
+                return true;
+            }
+            else
+            {
+                //MessageBox.Show("No user exists");//Debug
+                result.Close();
+                return false;
+            }
+        }
+
+        public string getPasswordHash(string user)
+        {
+            string hash = "";
+            string sql = "SELECT * from [dbo].[Users] where [username] = @user";
+            using SqlCommand command = new SqlCommand(sql, mainConnection);
+            command.Parameters.AddWithValue("@user", user);
+            var result = command.ExecuteReader();
+            if (result.HasRows)//Check if there is a result
+            {
+                result.Read();//Read the row
+                hash = result["password"].ToString();
+            }
+            result.Close();//close reader connection to insert when available
+            return hash;
+        }
+
+        public void RegisterUser(IDictionary<string, string> data)
+        {
+
+            String query = "INSERT INTO [dbo].[Users] (username,password,admin_powers,gluten_allergy,peanut_allergy,fish_allergy,soy_allergy,dairy_allergy,join_date,last_login) VALUES (@username,@password,@admin,@gluten,@peanut,@fish,@soy,@dairy,@join_date,@last_login)";
+
+            using SqlCommand command = new SqlCommand(query, mainConnection);
+            command.Parameters.AddWithValue("@username", data["username"]);
+            command.Parameters.AddWithValue("@password", data["password"]);
+            command.Parameters.AddWithValue("@admin", data["admin"]);
+            command.Parameters.AddWithValue("@gluten", data["gluten"]);
+            command.Parameters.AddWithValue("@peanut", data["peanut"]);
+            command.Parameters.AddWithValue("@fish", data["fish"]);
+            command.Parameters.AddWithValue("@soy", data["soy"]);
+            command.Parameters.AddWithValue("@dairy", data["dairy"]);
+            command.Parameters.AddWithValue("@join_date", DateTime.Now);
+            command.Parameters.AddWithValue("@last_login", DateTime.Now);
+
+            int result = command.ExecuteNonQuery();
+            // Check Error
+            if (result < 0)
+                MessageBox.Show("Error inserting data into Database!");
+            else
+                MessageBox.Show("Inserted user " + data["username"]);
+        }
+
+        public void makeAdmin(string user)
+        {
+            string sql = "UPDATE [dbo].[Users] SET [admin_powers] = '1' where [username] = @user";
+            using SqlCommand command = new SqlCommand(sql, mainConnection);
+            command.Parameters.AddWithValue("@user", user);
+            var result = command.ExecuteNonQuery();
+            if (result > 0)
+                MessageBox.Show("Updated " + user + " to be an administrator");
+            else
+                MessageBox.Show("Failed to make " + user + " an administrator");
+        }
+
+        public void updateLastLogin(string user)
+        {
+            string sql = "UPDATE [dbo].[Users] SET [last_login] = @last where [username] = @user";
+            using SqlCommand command = new SqlCommand(sql, mainConnection);
+            command.Parameters.AddWithValue("@last", DateTime.Now);
+            command.Parameters.AddWithValue("@user", user);
+            var result = command.ExecuteNonQuery();
+            if (result > 0)
+                MessageBox.Show("Updated " + user + " last login");//Debug
+            else
+                MessageBox.Show("Failed to update " + user + " last login");//Debug
+        }
+
+        public void changePassword(string user, string pass)
+        {
+            string sql = "UPDATE [dbo].[Users] SET [password] = @pass where [username] = @user";
+            using SqlCommand command = new SqlCommand(sql, mainConnection);
+            command.Parameters.AddWithValue("@pass", pass);
+            command.Parameters.AddWithValue("@user", user);
+            var result = command.ExecuteNonQuery();
+            if (result > 0)
+                MessageBox.Show("Updated " + user + " password");//Debug
+            else
+                MessageBox.Show("Failed to update " + user + " password");//Debug
+        }
+
+        public IDictionary<string, string> getUserData(string user)
+        {
+            //Prepare an associative data (key,pair)
+            IDictionary<string, string> data = new Dictionary<string, string>();
+
+            //Query DB for data
+            string sql = "SELECT * from [dbo].[Users] where [username] = @user";
+            using SqlCommand command = new SqlCommand(sql, mainConnection);
+            command.Parameters.AddWithValue("@user", user);
+            var reader = command.ExecuteReader();
+            if (reader.HasRows)//Check if there is a result
+            {
+                reader.Read();//Read the row
+
+                //Fill in the key,pairs
+                data["username"] = reader["username"].ToString();
+                data["admin_powers"] = reader["admin_powers"].ToString();
+                data["gluten_allergy"] = reader["gluten_allergy"].ToString();
+                data["peanut_allergy"] = reader["peanut_allergy"].ToString();
+                data["fish_allergy"] = reader["fish_allergy"].ToString();
+                data["soy_allergy"] = reader["soy_allergy"].ToString();
+                data["dairy_allergy"] = reader["dairy_allergy"].ToString();
+                data["join_date"] = reader["join_date"].ToString();
+                data["last_login"] = reader["last_login"].ToString();
+                reader.Close();
+                return data;
+            }
+            else throw new System.ArgumentException("No data available for user " + user);
+        }
+    }
+}
---
+++ Nutrition/Login.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Nutrition
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.usernameBox);
+            this.groupBox1.Location = new System.Drawing.Point(6, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 59);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Username";
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(6, 22);
+            this.usernameBox.MaxLength = 50;
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(131, 23);
+            this.usernameBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.passwordBox);
+            this.groupBox2.Location = new System.Drawing.Point(155, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(137, 59);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Password";
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(6, 22);
+            this.passwordBox.MaxLength = 100;
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = 'x';
+            this.passwordBox.Size = new System.Drawing.Size(125, 23);
+            this.passwordBox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Location = new System.Drawing.Point(9, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(303, 133);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Login";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 142);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+    }
+}
